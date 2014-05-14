d763a9310e9da0d6b4b1cdb1edef474db37e205a
Alumno <alumno@localhost.localdomain>
2014-05-14 07:48:12 -0600

    Modificaciones del ejercicio 2

---
+++ src/Ejercicio2.java
@@ -0,0 +1,23 @@
+
+import java.util.Scanner;
+public class Ejercicio2 {
+
+	public static void main(String[] args) {
+		//Codigo usado con lo aprendido en clase
+		//Este Programa lo que use para realizarlo es un while, asi de esta forma compara el texto y la va repitir
+		 // n cantidad de veces con la variable numero que esta es la que define cuantas veces se repetira.
+		 Scanner lea = new Scanner(System.in);  
+			System.out.print("Introduce un numero: ");
+			 int numero=0; 
+		        numero=lea.nextInt(); 
+		        System.out.println("Ingrese un Texto:  ");
+		    	String texto = lea.next();
+		    	int x=0;
+		    	while (x<numero)
+		    	{
+		    		System.out.println(texto);
+		    		x++;
+		    	}
+	}
+
+}
